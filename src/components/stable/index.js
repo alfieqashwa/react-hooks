fac92b98bed4549f45f9880a85969d8ae052aae6
alfieqashwa <alfieqashwa@gmail.com>
2019-03-13 07:20:28 +0700

    Added Stable Components

---
+++ src/components/stable/index.js
@@ -0,0 +1,9 @@
+import Blogpost from './Blogpost';
+import Clock from './Clock';
+import Counter from './Counter';
+import LoginControl from './LoginControl';
+import Page from './Page';
+import Reservation from './Reservation';
+import Toggle from './Toggle';
+
+export { Blogpost, Clock, Counter, LoginControl, Page, Reservation, Toggle };
