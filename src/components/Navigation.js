3f1c01235b8b00e1317211ddd120dd697f90856c
alfieqashwa <alfieqashwa@gmail.com>
2019-03-15 18:20:07 +0700

    styling --testing

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,29 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import { Flex, Box } from 'rebass';
+import { Nav, Box } from '../styles/Styles';
+// import { Flex, Box } from 'rebass';
 
 import * as ROUTES from '../constants/routes';
 
-const StyledLink = styled(Link)`
-  color: white;
-`;
-
 const Navigation = () => (
-  <Flex>
-    <Box p={3} width={1 / 2} color='white' bg='teal'>
-      <StyledLink to={ROUTES.HOME}>HOME</StyledLink>
+  <Nav>
+    <Box as={Link} to={ROUTES.HOME}>
+      HOME
     </Box>
-    <Box p={3} width={1 / 2} color='white' bg='teal'>
-      <StyledLink to={ROUTES.COUNTER}>Counter</StyledLink>
+    <Box as={Link} to={ROUTES.COUNTER}>
+      Counter
     </Box>
-    <Box p={3} width={1 / 2} color='white' bg='teal'>
-      <StyledLink to={ROUTES.CLOCK}>Clock</StyledLink>
+    <Box as={Link} to={ROUTES.CLOCK}>
+      Clock
     </Box>
-    <Box p={3} width={1 / 2} color='white' bg='teal'>
-      <StyledLink to={ROUTES.TICTACTOE}>TicTacToe</StyledLink>
+    <Box as={Link} to={ROUTES.TICTACTOE}>
+      TicTacToe
     </Box>
-  </Flex>
+  </Nav>
 );
 
 export default Navigation;
---
+++ src/styles/Styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: space-start;
+  border: 10px solid mistyrose;
+  min-height: '100vh';
+`;
+
+const Box = styled.div`
+  color: hotpink;
+  font-size: 20px;
+  text-align: center;
+  text-shadow: 4px 4px 0 rgba(0, 0, 0, 0.1);
+  padding: 10px;
+`;
+
+export { Nav, Box };
